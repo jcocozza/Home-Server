aed7ffdfffe77f52a87e5b35e3e24b6b13e643b7
Joseph Cocozza <jcocozza47@gmail.com>
2023-12-23 09:19:12 -0500

    feat: add speedtest services

--- services/speedtest/install.sh
+++ services/speedtest/install.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+#
+# Install the speedtest tooling
 
-if [[ $(uname -s) == "Darwin" ]]; then
+
+go_speedtest_version="v0.0.1-alpha"
+system="$(uname -s)"
+architecture="$(uname -m)"
+
+
+if [[ "$system" == "Darwin" ]]; then
+    # Install ookla
     brew tap teamookla/speedtest
     brew update
     brew install speedtest --force
-elif [[ $(uname -s) == "Linux" ]]; then
-    # Install speedtest
+
+    curl -LO https://github.com/jcocozza/go_speedtest/releases/download/$go_speedtest_version/go_speedtest_$system_$architecture
+    chmod +x go_speedtest_$system_$architecture
+elif [[ "$system" == "Linux" ]]; then
+    # Install ookla
     curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
     sudo apt-get install speedtest
+
+    # Install go_speedtest tool
+    curl -LO https://github.com/jcocozza/go_speedtest/releases/download/$go_speedtest_version/go_speedtest_$system
+    chmod +x go_speedtest_$system
 fi
