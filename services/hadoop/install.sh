9e092511cc00707e96c8d259d6ccc7927efe288c
Joseph Cocozza <jcocozza47@gmail.com>
2023-12-28 22:17:04 -0500

    feat: more hadoop work

--- services/hadoop/install.sh
+++ services/hadoop/install.sh
@@ -14,13 +14,7 @@ hosts=$(cat hosts.txt)
 source exports.sh
 #echo $HADOOP_USERNAME
 
-
-for ipaddr in "${machines[@]}"; do
-    # append to hosts file on the remote machines
-    read -p "Press Enter to add hosts for $ipaddr"
-    ssh "$BASE_USER@$ipaddr" "echo '$hosts' | sudo tee -a /etc/hosts > /dev/null"
-done
-
+echo "Adding Hadoop User to each machine..."
 for ipaddr in "${machines[@]}"; do
     # add the exports.sh file to the remote machine
     scp exports.sh "$BASE_USER@$ipaddr:exports.sh"
@@ -30,12 +24,50 @@ for ipaddr in "${machines[@]}"; do
     scp exports.sh "$HADOOP_USERNAME@$ipaddr:exports.sh"
 done
 
+read -p "Press Enter to hosts to .ssh/config file for passwordless login to hadoop user"
+ssh-keygen -t rsa -f ~/.ssh/id_tmp -N ''
+touch ~/.ssh/config
+for ipaddr in "${machines[@]}"; do
+    config="Host $ipaddr
+    HostName $ipaddr
+    IdentityFile ~/.ssh/id_tmp"
+
+    echo "$config" >> ~/.ssh/config
+
+    if ! ssh-copy-id -i ~/.ssh/id_tmp.pub  "$HADOOP_USERNAME@$ipaddr"; then
+        echo "Failed to copy SSH key to $ipaddr"
+    fi
+done
+
+
+for ipaddr in "${machines[@]}"; do
+    # append to hosts file on the remote machines
+    read -p "Press Enter to add hosts for $ipaddr"
+    ssh -t "$HADOOP_USERNAME@$ipaddr" "echo '$hosts' | sudo tee -a /etc/hosts > /dev/null"
+done
+
+
 read -p "Press Enter to begin second process"
 keyfile="/home/$HADOOP_USERNAME/.ssh/hadoop"
+
+install_hadoop() {
+    local ip="$1"
+
+    # Execute install on the remote machine
+    ssh -t "$HADOOP_USERNAME@$ip" '
+        sudo apt-get update;
+        sudo apt install -y openjdk-11-jdk;  # Using '-y' for non-interactive installation
+        source exports.sh;
+        bash -s < /tmp/install.sh;
+        sudo mv hadoop '"$HADOOP_LOCATION"';
+    '
+}
+
 for ipaddr in "${machines[@]}"; do
     read -p "Press Enter to share keys for $ipaddr"
     # share public keys with all other machines on the cluster
     # send key to all other ips
+
     ssh "$BASE_USER@$ipaddr" "source exports.sh; sudo -u $HADOOP_USERNAME ssh-keygen -t rsa -f $keyfile -N ''; sudo chmod 600 $keyfile"
     for ip in "${machines[@]}"; do
         if [[ "$ip" != "$ipaddr" ]]; then
@@ -43,13 +75,15 @@ for ipaddr in "${machines[@]}"; do
         fi
     done
 
-    read -p "Press Enter to install hadoop for $ipaddr"
     # install hadoop
-    ssh -t "$HADOOP_USERNAME@$ipaddr" "sudo apt-get update;"
-    ssh -t "$HADOOP_USERNAME@$ipaddr" "sudo apt install openjdk-11-jdk;"
-    ssh "$HADOOP_USERNAME@$ipaddr" "source exports.sh; bash -s" < "services/hadoop/local/install.sh"
-    ssh "$HADOOP_USERNAME@$ipaddr" "sudo mv hadoop $HADOOP_LOCATION"
+    read -p "Press Enter to install hadoop for $ipaddr"
+    # Copy the local script to the remote machine
+    scp services/hadoop/local/install.sh "$HADOOP_USERNAME@$ipaddr:/tmp/"
+
+    install_hadoop "$ipaddr" &
 done
+
+wait
 
 #ssh "$HADOOP_USERNAME@$HADOOP_MASTER_IP" "bash -s" < "services/hadoop/master/setup.sh"
 # Setup hadoop
@@ -57,8 +91,14 @@ read -p "Press Enter to setup config for master $HADOOP_MASTER_IP"
 ssh "$HADOOP_USERNAME@$HADOOP_MASTER_IP" "source exports.sh; bash -s" < "services/hadoop/master/setup/config_setup.sh"
 
 for ipaddr in "${machines[@]}"; do
-    ssh "$HADOOP_USERNAME@$ipaddr" "sudo chown $HADOOP_USERNAME:root -R $HADOOP_LOCATION"
-    ssh "$HADOOP_USERNAME@$ipaddr" "sudo chmod g+rwx -R $HADOOP_LOCATION"
+
+    ssh -t "$HADOOP_USERNAME@$ipaddr" '
+        sudo chown '"$HADOOP_USERNAME"':root -R '"$HADOOP_LOCATION"';
+        sudo chmod g+rwx -R "'$HADOOP_LOCATION'"
+    '
+
+    #ssh "$HADOOP_USERNAME@$ipaddr" "sudo chown $HADOOP_USERNAME:root -R $HADOOP_LOCATION"
+    #ssh "$HADOOP_USERNAME@$ipaddr" "sudo chmod g+rwx -R $HADOOP_LOCATION"
 done
 
 read -p "Press Enter to distribute configuration to ${machines[*]}"
